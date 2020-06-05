5b602ae4f7fd52ffb1e092974f4ec7f614281ac1
Lena Kovaleva <insolence14@gmail.com>
2020-06-05 23:17:02 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,217 @@
+export let htmlCollection = [
+	["HTML Cheat Sheet", "https://www.wpkube.com/html5-cheat-sheet/"],
+	["htmlbook", "http://htmlbook.ru/"],
+	["Where to learn HTML and CSS", "https://css-tricks.com/where-do-you-learn-html-css-in-2019/"],
+	["HTML attribute reference", "https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes"],
+	["HTML elements reference","https://developer.mozilla.org/en-US/docs/Web/HTML/Element"],
+	
+
+];
+
+export let cssCollection = [
+	["Свойство justify-content", "https://basicweb.ru/css/css_pr_justify-content.php"],
+	["Руководство по CSS", "https://developer.mozilla.org/ru/docs/Web/CSS/Reference"],
+	["Can I Use", "https://caniuse.com/#feat=flexbox"],
+	["Генератор кнопок", "https://maxzon.ru/button_gen/index.html"],
+	["Селекторы, псевдоклассы и псевдоэлементы", "https://basicweb.ru/css/css_selectors.php"],
+	["Bootstrap Navbar Center Links", "https://codepen.io/akpro/pen/ERJKBx"],
+	["CSS Gradients", "https://html5book.ru/css3-gradient/"],
+	["Псевдоклассы и псевдоэлементы CSS", "https://idg.net.ua/blog/uchebnik-css/azy-css/psevdoklassy-psevdoelementy-selektory-potomkov"],
+	["Как сделать ломаный фон?", "https://ru.stackoverflow.com/questions/823891/%D0%9A%D0%B0%D0%BA-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D0%BB%D0%BE%D0%BC%D0%B0%D0%BD%D1%8B%D0%B9-%D1%84%D0%BE%D0%BD?rq=1"],
+	["Media Queries", "https://idg.net.ua/blog/uchebnik-css/razmetka-css/adaptivnyj-dizajn/media-queries"],
+	["Генератор Flexbox верстки", "https://basicweb.ru/css/flexbox_generator.php"],
+	["CSS Flexible Box Layout Module", "https://www.w3.org/TR/css-flexbox-1/"],
+	["A Complete Guide to Grid", "https://css-tricks.com/snippets/css/complete-guide-grid/"],
+	["Learn CSS Grid", "https://learncssgrid.com/"],
+	["Grid vs Flexbox", "https://medium.com/@stasonmars/%D1%80%D0%B5%D1%88%D0%B0%D1%8E%D1%89%D0%B0%D1%8F-css-%D0%B1%D0%B8%D1%82%D0%B2%D0%B0-grid-%D0%BF%D1%80%D0%BE%D1%82%D0%B8%D0%B2-flexbox-8fee99ab6cbd"],
+	["CSS Grid и Flexbox: сравнение на практике", "http://htmlbook.ru/blog/css-grid-i-flexbox-sravnenie-na-praktike"],
+	["Генератор Grid верстки", "https://basicweb.ru/css/grid_generator.php"],
+	["Знакомство с LESS", "https://htmlacademy.ru/courses/85"],
+	["SASS Guidelines", "https://sass-guidelin.es/ru/"],
+	["Getting started with Sass and Compass", "http://thesassway.com/beginner/getting-started-with-sass-and-compass"],
+	["SASS @mixin and @include", "https://sass-lang.com/documentation/at-rules/mixin"],
+	["Nine Ways to Write the Best SASS Code", "https://opensenselabs.com/blog/tech/nine-ways-write-best-sass-code"],
+	["Градиент для текста", "https://html5book.ru/gradient-dlya-teksta/"],
+
+];
+
+export let jsCollection = [
+	["Учебник learn.javascript.ru", "https://learn.javascript.ru/"],
+	["Учите Vanilla JS", "https://www.cat-in-web.ru/vanilla-js/"],
+	["Чем отличаются JavaScript и ECMAScript?", "https://habr.com/ru/company/nix/blog/342904/"],
+	["Owl Carousel 2", "https://owlcarousel2.github.io/OwlCarousel2/"],
+
+];
+
+export let fontsCollection = [
+	["FontAwesome", "https://fontawesome.com/"],
+	["Google Fonts", "https://fonts.google.com/"],
+];
+
+export let colorsCollection = [
+	["Color Picker", "https://qhmit.com/css/css_color_codes.cfm"],
+	["Adobe Color Wheel", "https://color.adobe.com/ru/create/color-wheel/"],
+	["Конвертер цветов + названия на HTML", "https://basicweb.ru/html/html_colors.php"],
+	["Hex Values", "http://online.sfsu.edu/chrism/hexval.html"],
+	["Color Scheme", "https://colorscheme.ru/"],
+	["Palettable", "https://palettable.io/C7B188"],
+];
+
+export let iconsCollection = [
+	["Tilda icons", "https://tilda.cc/free-icons/"],
+	["Icon8", "https://icons8.com/icons"],
+	["Iconmonstr", "https://iconmonstr.com/"],
+];
+
+export let emmetCollection = [
+	["Emmet Cheat Sheet", "https://docs.emmet.io/cheat-sheet/"],
+	["Сокращения", "https://webdesign-master.ru/blog/html-css/2.html"],
+];
+
+export let picturesCollection = [
+	["Free Logo Design", "https://www.freelogodesign.org/"],
+	["Photo Resizer", "https://www.photoresizer.com/"],
+];
+
+export let templatesCollection = [
+	["Где взять макеты для верстки?", "https://qna.habr.com/q/214580"],
+	["Behance", "http://behance.net/"],
+	["Dribbble", "https://dribbble.com/"],
+	["Template Monster", "https://www.templatemonster.com/ru/"],
+];
+
+export let designCollection = [
+	["Visual Design Course", "https://skillcrush.com/blueprint/visual-design-course/"],
+	["", ],
+];
+
+export let inspirationCollection = [
+	["Как читать медиа?", "https://howtoreadmedia.ru/ru/"],
+	["Citationsy", "https://citationsy.com/"],
+	["Проекты Ильи Бирмана", "https://ilyabirman.ru/projects/"],
+	["Бюро Горбунова", "https://bureau.ru/"],
+	["Студия Артемия Лебедева", "https://www.artlebedev.ru/"],
+	["Shades colors, design", "https://www.elegantthemes.com/blog/wordpress/how-to-comment-your-code-like-a-pro-best-practices-and-good-habits"],
+	["", ],
+];
+
+export let gitCollection = [
+	["Git happens! 6 типичных ошибок Git и как их исправить", "https://habr.com/ru/company/flant/blog/419733/"],
+	["Как научить людей использовать Git", "https://habr.com/ru/post/437000/"],
+	["Скринкаст по Git от Ильи Кантора", "https://www.youtube.com/playlist?list=PLDyvV36pndZHkDRik6kKF6gSb0N0W995h"],
+
+];
+
+export let articlesCollection = [
+	["Фронтенд — это не больно!", "https://bespoyasov.ru/front-not-pain/#you-know-your-shit"],
+	["How to be great at asking coding questions", "https://medium.com/@gordon_zhu/how-to-be-great-at-asking-questions-e37be04d0603"],
+	["Web storage overview", "https://developers.google.com/web/fundamentals/instant-and-offline/web-storage/"],
+	["Синхронизация окон браузеров", "https://ydmitry.ru/blog/ray-dlya-verstalshchika-sinkhronizatsiya-okon-brauzerov/"],
+	["Поиск на сайте", "https://habr.com/ru/post/244561/"],
+	["Как сделать поиск по сайту?", "http://shpargalkablog.ru/2011/06/forma-poiska-po-saitu.html"],
+	["Can I Use That Picture? 3 Rules to Ensure Your Blog Isn’t Breaking Copyright Rules", "https://mediablog.prnewswire.com/2017/03/29/blogging-copyright-issues/"],
+	["What is cached data?", "https://www.freecodecamp.org/news/what-is-cached-data/"],
+	["Google API", "https://habr.com/ru/post/110460/"],
+];
+
+export let learningCollection = [
+	["Что должен уметь разработчик?", "https://habr.com/ru/company/netologyru/blog/327294/"],
+	["Путь верстальщика: с нуля до сеньора", "https://habr.com/ru/post/346010/"],
+	["Карта развития веб-разработчика", "https://github.com/zualex/devmap"],
+	["Web Skills by Andreas", "https://andreasbm.github.io/web-skills/?compact"],
+];
+
+export let seoCollection = [
+	["", ],
+	["", ],
+];
+
+export let toolsCollection = [
+	["Codepen", "https://codepen.io/"],
+	["Validator W3C", "https://validator.w3.org/"],
+	["Инструменты проверки качества работы сайта", "https://sitecoder.blogspot.com/2015/10/how-to-check-quality-work-freelancer-or.html"],
+	["Проверка оптимизации для мобильных устройств", "https://search.google.com/test/mobile-friendly?utm_source=mft&utm_medium=redirect&utm_campaign=mft-redirect"],
+	["Plunker", "https://plnkr.co/?utm_source=legacy&utm_medium=worker&utm_campaign=next"],
+	["WriteMaps", "https://writemaps.com/"],
+	["CSS autoprefixer", "https://github.com/postcss/autoprefixer"],
+	["Compass. CSS Framework", "http://compass-style.org/"],
+	["Placeholder.com", "http://compass-style.org/"],
+	["Placeholder with kittens", "http://placekitten.com/"],
+
+];
+
+export let nodejsCollection = [
+	["", ],
+	["", ],
+];
+
+export let npmCollection = [
+	["npm для простых смертных", "https://habr.com/ru/post/243335/"],
+	["", ],
+];
+
+export let bundlersCollection = [
+	["Сборка проекта с помощью Gulp", "https://habr.com/ru/post/250569/"],
+	["", ],
+];
+
+export let animationCollection = [
+	["Как правильно использовать анимацию в дизайне интерфейсов", "https://proglib.io/p/kak-pravilno-ispolzovat-animaciyu-v-dizayne-interfeysov-2020-04-29"],
+	["", ],
+];
+
+export let algorithmsollection = [
+	["Зачем нужны алгоритмы и паттерны", "https://htmlacademy.ru/blog/boost/frontend/short-15"],
+	["Sorting Algorithms Animations", "https://www.toptal.com/developers/sorting-algorithms"],
+	["Паттерны ООП в метафорах", "https://habr.com/ru/post/136766/"],
+];
+
+export let booksCollection = [
+	["Design Patterns: Elements of Reusable Object-Oriented Software by Erich Gamma, Richard Helm, Ralph Johnson, and JohnVlissides", "http://wiki.c2.com/?DesignPatternsBook"],
+	["Learning JavaScript Design Patterns by Addy Osmani", "https://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript"],
+];
+
+export let coursesCollection = [
+	["Codecademy", "https://www.codecademy.com/"],
+	["w3schools", "https://www.w3schools.com/html/html_intro.asp"],
+	["Learn to code. HTML & CSS","https://learn.shayhowe.com/html-css/"],
+	["Web Tutorials. Tizag.com", "http://www.tizag.com/htmlT/htmldiv.php"],
+	["Web Tutorials. w3.org", "https://www.w3.org/WAI/tutorials/images/decision-tree/"],
+	["Chris Courses", "https://chriscourses.com/courses"],
+	["HTML, CSS, JS, database tutorials", "https://qhmit.com/"],
+	["Codewars", "https://www.codewars.com/"],
+	["Freecodecamp", "https://www.freecodecamp.org/"],
+	["Hexlet", "https://ru.hexlet.io/"],
+	["Watch and Code", "https://watchandcode.com/p/practical-javascript"],
+	["Wes Bos Courses", "https://wesbos.com/courses/"],
+	["Guides to Web Design and Development", "https://www.smashingmagazine.com/guides/"],
+	["WayUp", "https://wayup.in/"],
+	["Тонкости верстки on Coursera", "https://www.coursera.org/learn/tonkosti-verstki"],
+];
+
+export let youtubeCollection = [
+	["", ],
+	["", ],
+];
+
+export let compilationsCollection = [
+	["64 сайта для дизайнеров", "https://skillbox.ru/media/design/64_sayta_dlya_dizaynerov/"],
+	["Russian-it, github compilation", "https://github.com/unchase/awesome-russian-it/blob/master/YouTube.md#youtube-development-web"],
+	["UX Resource list", "https://blog.prototypr.io/my-ux-resource-list-96ab9e36ac24"],
+	["50 Useful Tools and Resources For Web Designers", "https://www.smashingmagazine.com/2010/07/50-useful-tools-and-resources-for-web-designers/"],
+];
+
+export let sublimeCollection = [
+	["30+ Sublime Text 3 shortcuts", "https://dev.to/weeb/30-of-the-most-useful-sublime-text-3-shortcuts-4d0p#ctrl-shift-up-down"],
+	["DeclareThatColor Sublime Extension", "https://github.com/bertdida/DeclareThatColor"],
+];
+
+export let resourcesCollection = [
+	["Tproger", "https://tproger.ru/tag/javascript/"],
+	["", ],
+];
+
+export let commentsCollection = [
+	["JSDoc", "https://jsdoc.app/"],
+
+];
---
+++ js/script.js
@@ -0,0 +1,72 @@
+import { htmlCollection, cssCollection, jsCollection, fontsCollection, colorsCollection, iconsCollection, emmetCollection, picturesCollection, templatesCollection, designCollection, inspirationCollection, gitCollection, articlesCollection, learningCollection, seoCollection, toolsCollection, nodejsCollection, npmCollection, bundlersCollection, animationCollection, algorithmsollection, booksCollection, coursesCollection, youtubeCollection, compilationsCollection, sublimeCollection, resourcesCollection, commentsCollection } from "./data.js";
+
+let Collections = [htmlCollection, cssCollection, jsCollection, fontsCollection, colorsCollection, iconsCollection, emmetCollection, picturesCollection, templatesCollection, designCollection, inspirationCollection, gitCollection, articlesCollection, learningCollection, seoCollection, toolsCollection, nodejsCollection, npmCollection, bundlersCollection, animationCollection, algorithmsollection, booksCollection, coursesCollection, youtubeCollection, compilationsCollection, sublimeCollection, resourcesCollection, commentsCollection]; // all arrays collected
+
+let idCollection = ["html", "css", "js", "fonts", "colors", "icons", "emmet", "pictures", "templates", "design/UX", "inspiration", "git", "articles", "learning path", "seo", "tools for dev", "node.js", "npm", "bundlers", "animation", "Algorithms & OOP", "books", "courses", "youtube", "compilations", "sublime", "resources", "comments"]; // all ids collected
+
+var section = document.createElement("section");
+
+var container = document.createElement("div");
+container.setAttribute("class", "container");
+section.appendChild(container);
+
+var row = document.createElement("div");
+row.setAttribute("class", "row");
+container.appendChild(row);
+
+var col = document.createElement("div");
+col.setAttribute("class", "col-12");
+row.appendChild(col);
+
+var name = document.createElement("h2");
+var nameText = document.createTextNode("Resources Compilation");
+name.appendChild(nameText);
+section.appendChild(name);
+document.body.appendChild(section);
+
+
+for (let i = 0; i < idCollection.length; i++) {
+
+    var section = document.createElement("section");
+    var id = idCollection[i];
+    section.setAttribute("class", id);
+
+    var container = document.createElement("div");
+    container.setAttribute("class", "container");
+    section.appendChild(container);
+
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+    container.appendChild(row);
+
+    var col = document.createElement("div");
+    col.setAttribute("class", "col-12");
+    row.appendChild(col);
+
+    var ul = document.createElement("ul");
+    ul.setAttribute("id", id);
+    col.appendChild(ul);
+
+    var title = document.createElement("h1");
+    var titleId = document.createTextNode(id);
+    title.appendChild(titleId);
+    ul.appendChild(title);
+    document.body.appendChild(section);
+
+
+    for (var k = 0; k < Collections[i].length; k++) {
+        var text = document.createTextNode(Collections[i][k][0]);
+
+        console.log(Collections[i][k][1]);
+
+        var a = document.createElement("a");
+        a.appendChild(text);
+        a.href = Collections[i][k][1];
+        var li = document.createElement("li");
+        li.appendChild(a);
+
+        var ul = document.getElementById([id]);
+        ul.appendChild(li);
+
+    }
+}
